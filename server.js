6fd4b3cb8e8cbd8ec1ca50c06f99c4a641688e40
Mike Cornish <cornishmw@gmail.com>
2016-09-25 23:47:22 -0400

    App working as expected

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const timestamp = require('unix-timestamp');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/:stamp', (req, res) => {
+    const stamp = req.params.stamp;
+    const result = {
+        "unix": null,
+        "natural": null
+    };
+
+    if (_isNatural(stamp)) {
+        const natural = new Date(stamp);
+        result.natural = _formatDate(natural);
+        result.unix = _getUnix(natural);
+    } else if (_isUnix(stamp)) {
+        const unix = +stamp;
+        result.unix = unix;
+        result.natural = _formatDate(_getNatural(unix));
+    }
+
+    res.json(result);
 });
 
 app.listen(8080, () => {
     console.log('App listening on :8080!');
 });
+
+function _isNatural(stamp) {
+    // Detect valid date. Idea from: http://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript
+    const timestamp = Date.parse(stamp);
+    return !isNaN(timestamp);
+}
+
+function _isUnix(stamp) {
+    const natural = timestamp.toDate(+stamp);
+    return natural instanceof Date;
+}
+
+function _getNatural(unix) {
+    return timestamp.toDate(unix);
+}
+
+function _getUnix(natural) {
+    return timestamp.fromDate(natural);
+}
+
+function _formatDate(date) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
